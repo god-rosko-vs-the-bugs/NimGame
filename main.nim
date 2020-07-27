10b7ed117da2fdcf4fecb93a062acb3b002d15c6
Rosen Popov <rdonkow@gmail.com>
2020-07-27 18:13:45 +0300

    new main

--- main.nim
+++ main.nim
@@ -2,42 +2,44 @@ import nico
 import random
 # frame counter
 var frame = 0
-var size_x = 128
-var size_y = 128
+var size_x =64 
+var size_y =64 
 
 type
-  ball = object
-    x,y: int
-type
-  mvector = object
-    x,y: float
+  Matrix[W, H: static[int]] = object
+    data:array[1..W, array[1..H, int]]
 
-var movment: mvector
-var obj_ball:ball
+var tet: Matrix[25,10]
+var loc_x:int=0
+var loc_y:int=0
+proc DrawScreen(board:Matrix)=
+  for i in 1..board.H:
+    for j in 1..board.W:
+      nico.setColor(board.data[j][i])
+      nico.pset(20+i,20+board.W-j);
+
 proc gameInit() =
-  obj_ball.x = size_x div 2
-  obj_ball.y = size_y div 2
-  movment.x = 2
-  movment.y = 1
+  loc_x = 1
+  loc_y = 1
 
 proc gameUpdate(dt: Pfloat) =
-  obj_ball.x = (obj_ball.x + movment.x).int
-  obj_ball.y = (obj_ball.y + movment.y).int
-  if obj_ball.x <= 0 or obj_ball.x >= size_x:
-    movment.x = -movment.x 
-  if obj_ball.y <= 0 or obj_ball.y >= size_y:
-    movment.y = -movment.y
-
+  if frame mod 10 == 0:
+    tet.data[loc_x][loc_y] = rand(1..14)
+    loc_y+=1
+    if loc_y == 10:
+      loc_x+=1
+      loc_y=1
+      if loc_x mod 25 == 0:
+        loc_y=1
+        loc_x=1
+  frame+=1
 proc gameDraw() =
-  #cls()
-  setColor(10)
-  pset(obj_ball.x,obj_ball.y)
-# initialization
-nico.init("nico", "test")
+  nico.cls()
+  DrawScreen(tet)
 
-# we want a fixed sized screen with perfect square pixels
+nico.init("nico", "test")
 integerScale(true)
-nico.createWindow("nico",size_x,size_y,4)
+nico.createWindow("nico", size_x, size_y, 8)
 
 nico.run(gameInit, gameUpdate, gameDraw)
 
