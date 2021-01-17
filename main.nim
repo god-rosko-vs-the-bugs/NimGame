00f32ac86fedde20d50a8938c146b5a582de12c1
Rosen Popov <rdonkow@gmail.com>
2021-01-17 02:50:11 +0200

    Tmporary commit: save changes
    Started writing primitive 2d engine in nim based on nico
    very initioal stages
    might include a sprite editor later

--- main.nim
+++ main.nim
@@ -1,20 +1,91 @@
 import nico
+import tables
 import random
+import sugar
 # frame counter
 var frame = 0
-var size_x =64 
-var size_y =64 
+const X_Dim =64 
+const Y_Dim =64 
+const Scale_Fac =  8
 
-type
-  Matrix[W, H: static[int]] = object
-    data:array[1..W, array[1..H, int]]
+type 
+  Screen =  ref object 
+    matr:seq[seq[int]] 
+  SpriteLayers  = ref object
+    planes :seq[Screen]
+  SubSprite = ref object
+    X_Up,Y_Up:int
+    layer:int
+    repr:Screen
 
-var tet: Matrix[25,10]
-var loc_x:int=0
-var loc_y:int=0
-proc DrawScreen(board:Matrix)=
-  for i in 1..board.H:
-    for j in 1..board.W:
+#var GlobalSprites:seq[SubSprite]
+#var GlobalSpritesTable:Table[string,SubSprite]
+
+proc CreateBlankScreen(x_dim:int = X_Dim,y_dim:int = Y_Dim):Screen=
+  var tmp : Screen
+  tmp.matr.setLen(x_dim)
+  for i in 0..tmp.matr.high() :
+    tmp.matr[i].setLen(y_dim)
+  return tmp 
+
+proc CleanSector(pl:var Screen,rectangle:array[4,int] = [0,0,0,0])=
+  var x_low  = rectangle[0]
+  var x_high = rectangle[2] 
+  var y_low  = rectangle[1]
+  var y_high = rectangle[3]
+
+  if rectangle == [0,0,0,0]:
+    x_low = pl.matr.low()
+    y_low = pl.matr[0].low()
+    x_high = pl.matr.high()
+    y_high = pl.matr[0].high()
+
+  for x in x_low..x_high:
+    for y in x_low..x_high:
+      pl.matr[x][y] = 0
+  return
+
+proc MeldSpriteLayers(main_scr:var Screen, layers: ref SpriteLayers)=
+  var la:int = layers.planes.len() - 1
+  var full:bool = true
+  while la > 0:
+    full = true
+    for x in 0..layers.planes[0].matr.high():
+      for y in 0..layers.planes[0].matr[0].high():
+        if main_scr.matr[x][y] == 0:
+          full = false
+          main_scr.matr[x][y] = layers.planes[la].matr[x][y] 
+    if full:
+      break
+    dec(la)
+
+proc InsertToLayer(layers: ref SpriteLayers, spr:SubSprite, layer:int,ofs:array[2,int]=[0,0])=
+  const x = 0
+  const y = 1
+   
+  var OriSprite:array[2,int] = [spr.X_Up + ofs[x],spr.Y_Up + ofs[y] ]
+  var EndSprite:array[2,int] = [OriSprite[x] + spr.repr.matr.high(),
+                                OriSprite[y] + spr.repr.matr[0].high() ]
+  
+  var OriBound:array[2,int] = [max]
+  var EndBound:array[2,int]
+  
+
+  for x in 0..layers.planes[layer].matr.high():
+    for y in 0..layers.planes[layer].matr[0].high():
+      if 
+  
+
+  
+  
+
+  return
+
+func InsertToLayer(layers: ref SpriteLayers, spr:SubSprite) = 
+
+
+proc DrawScreen(board:Screen)=
+  
       nico.setColor(board.data[j][i])
       nico.pset(20+i,20+board.W-j);
 
@@ -39,7 +110,7 @@ proc gameDraw() =
 
 nico.init("nico", "test")
 integerScale(true)
-nico.createWindow("nico", size_x, size_y, 8)
+nico.createWindow("nico", X_Dim, Y_Dim, Scale_Fac)
 
 nico.run(gameInit, gameUpdate, gameDraw)
 
